0f9f56c83dff247e7b97be01c354c5b321c8d6ae
Nagunt <reiy0902@naver.com>
2020-01-30 18:18:50 +0900

    First Commit

---
+++ Assets/Move.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+
+    float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+            transform.position += new Vector3(0, Time.deltaTime * 0.01f, 0);
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.name == "Enemy")
+            Debug.Log("Dead");
+
+    }
+}
